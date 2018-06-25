5f432e4667d1fe75dc75645e1f9e141d4adbf2fa
Alexandre Peyron <alpeyron@gmail.com>
2018-06-25 12:05:03 +0200

    update TransactionController and view

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionsController extends Controller
 {
     public function index()
     {
+        $transactions = Transaction::latest()->get();
 
+        return view('transactions.index', compact('transactions'));
     }
 }
---
+++ resources/views/transactions/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts/app')
+
+<div class="container">
+    <div class="table-responsive">
+        <table class="table">
+            plop
+            <thead>
+                <tr>
+                    <td>Date</td>
+                    <td>Description</td>
+                    <td>Montant</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($transactions as $transaction)
+                    <tr>
+                        <td>{{ $transaction->created_at->format('d-m-Y') }}</td>
+                        <td>{{ $transaction->description }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
