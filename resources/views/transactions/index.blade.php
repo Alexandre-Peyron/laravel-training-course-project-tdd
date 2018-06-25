ce10b864a80536a8456db37b7a338ff3ddfe7574
Alexandre Peyron <alpeyron@gmail.com>
2018-06-25 12:07:32 +0200

    update view

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -1,24 +1,26 @@
 @extends('layouts/app')
 
-<div class="container">
-    <div class="table-responsive">
-        <table class="table">
-            plop
-            <thead>
-                <tr>
-                    <td>Date</td>
-                    <td>Description</td>
-                    <td>Montant</td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($transactions as $transaction)
+@section('content')
+    <div class="container">
+        <div class="table-responsive">
+            <table class="table">
+                plop
+                <thead>
                     <tr>
-                        <td>{{ $transaction->created_at->format('d-m-Y') }}</td>
-                        <td>{{ $transaction->description }}</td>
+                        <td>Date</td>
+                        <td>Description</td>
+                        <td>Montant</td>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($transactions as $transaction)
+                        <tr>
+                            <td>{{ $transaction->created_at->format('d-m-Y') }}</td>
+                            <td>{{ $transaction->description }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
+@endsection
