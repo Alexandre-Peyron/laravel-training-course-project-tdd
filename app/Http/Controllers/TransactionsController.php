60990bf76921780e9cc6bcafd96ab573d93c59b6
Alexandre Peyron <alpeyron@gmail.com>
2018-06-25 15:09:14 +0200

    refacto transaction index

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -8,14 +8,9 @@ use Illuminate\Http\Request;
 
 class TransactionsController extends Controller
 {
-    public function index(Category $category = null)
+    public function index(Category $category)
     {
-        if ($category && $category->exists) {
-            $transactions = $transactions = Transaction::latest()->where('category_id', $category->id)->get();
-        }
-        else {
-            $transactions = Transaction::latest()->get();
-        }
+        $transactions = Transaction::byCategory($category)->get();
 
         return view('transactions.index', compact('transactions'));
     }
--- app/Transaction.php
+++ app/Transaction.php
@@ -6,8 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * @param          $query
+     * @param Category $category
+     */
+    public function scopeByCategory($query, Category $category)
+    {
+        if ($category->exists) {
+            $query->where('category_id', $category->id);
+        }
     }
 }
